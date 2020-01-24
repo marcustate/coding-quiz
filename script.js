5754a739d610d3af566bd6e5e6335edc28d609cd
Marcus Tate <bigmark0219@gmail.com>
2020-01-23 18:38:06 -0600

    pushing code

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
+var submitAnswer = function() {
 
+    var radios = document.getElementsByName('choice');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+    
+    if (val == "" ) {
+      alert('please select choice answer');
+    } else if ( val == "Scripting") {
+      alert('Answer is correct!!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
+
+
+
+  var submitAnswer2 = function() {
+
+    var radios = document.getElementsByName('choice');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+    
+    if (val == "" ) {
+      alert('please select choice answer');
+    } else if ( val == "Document Object Model") {
+      alert('Answer is correct!!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
+
+  var submitAnswer3 = function() {
+
+    var radios = document.getElementsByName('choice');
+    var val= "";
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+           val = radios[i].value; 
+           break;
+         }
+    }
+    
+    if (val == "" ) {
+      alert('please select choice answer');
+    } else if ( val == "Bootstrap") {
+      alert('Answer is correct!!');
+    } else {
+      alert('Answer is wrong');
+    }
+  };
